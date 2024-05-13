82848b46d150c848de5982436efc879f8ed1b47d
subrata mondal <subratamondal1029@outlook.com>
2024-05-13 20:23:29 +0530

    adding Some files for Naviagte The project

---
+++ projectData.js
@@ -0,0 +1,51 @@
+const projectData = [
+    {
+        projectName : "Color Changer",
+        projectDescription : "This is a simple color changer app that changes the background color of the page on click of a button.",
+        folderName : "color_changer",
+        projectImage : "color_changer.png"
+    },
+    {
+        projectName : "BMI Calculator",
+        projectDescription : "This is a BMI calculator that calculate BMI using height and weight",
+        folderName : "BMI_calculator",
+        projectImage : "Bmi_calculator.png"
+    },
+    {
+        projectName : "Digital Clock",
+        projectDescription : "This is a Digital clock That shows the local time",
+        folderName : "Digital_clock",
+        projectImage : "Digital_clock.png"
+    },
+    {
+        projectName : "Guess The Number",
+        projectDescription : "This is a Game to Guess The Random Number",
+        folderName : "GuessTheNumber",
+        projectImage : "GuessTheNumber.png"
+    },
+    {
+        projectName : "Keyboard Track",
+        projectDescription : "This Keyboard Key tracker That shows the key and keyCode",
+        folderName : "keyboard_track",
+        projectImage : "keyboard_track.png"
+    },
+    {
+        projectName : "Scroll Indicator",
+        projectDescription : "This Scroll Indicator That shoes the scrolling progress",
+        folderName : "scroll_indicator",
+        projectImage : "scroll_indicator.png"
+    },
+    {
+        projectName : "Automatic Background Changer",
+        projectDescription : "This is change The Background color every 1 second when start button will click and stop changing Background color",
+        folderName : "BgColor_changer_auto",
+        projectImage : "BG_color_auto.png"
+    },
+    {
+        projectName : "Typer",
+        projectDescription : "This is Automatic Text Typer That print And Erase Word letter by letter",
+        folderName : "Typer",
+        projectImage : null
+    },
+
+]
