f8ea50b68a3b91204e4e74dbff00ec57aaa6cd26
subrata mondal <subratamondal1029@outlook.com>
2024-06-01 18:51:03 +0530

    deleting a project

--- scroll_indicator/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-  const webHeight = document.documentElement.scrollHeight;
-  const windowHeight = document.documentElement.clientHeight;
-  const scrollUpto = document.documentElement.scrollTop;
-
-  const scrolledParsentage = (scrollUpto / (webHeight - windowHeight)) * 100;
-  document.querySelector(".progress").style.width = scrolledParsentage + "%";
-});
