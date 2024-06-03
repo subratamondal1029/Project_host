6557cda9d60c08e519fb5078be52a70a7b1b5b3c
subrata mondal <subratamondal1029@outlook.com>
2024-06-03 22:55:27 +0530

    all complete with sharing option

--- quiz_app/script.js
+++ quiz_app/script.js
@@ -284,7 +284,29 @@ function showResult(){
             <img src="images/Instagram.png" alt="Instagram">
         </div>
     </div>
-</div>`
+
+<div class="card" id="popUp">
+<div class="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><g stroke-width="0" id="SVGRepo_bgCarrier"></g><g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g><g id="SVGRepo_iconCarrier"> <path stroke-linejoin="round" stroke-linecap="round" stroke-width="1.5" stroke="#ffffff" d="M20 14V17.5C20 20.5577 16 20.5 12 20.5C8 20.5 4 20.5577 4 17.5V14M12 15L12 3M12 15L8 11M12 15L16 11"></path> </g></svg>
+</div>
+<div class="content">
+    <span class="title">Unsupported browser</span>
+    <div class="desc">This Feature is not available for Your Browser, Do you Want to download Your result as Image.</div> 
+    <div class="actions">
+        <div>
+            <a href="#" class="download" id="downloadResult">Download</a>
+        </div>
+        <div>
+            <a href="#" class="notnow popClose">Not now</a>
+        </div>
+    </div>    
+</div>
+<button type="button" class="close popClose">
+    <svg aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+</button>
+</div>
+</div>
+`
 
 const heighestScore = getLocalData('highestScore')
 if (!heighestScore || heighestScore < correctAnswers) {
@@ -292,6 +314,7 @@ if (!heighestScore || heighestScore < correctAnswers) {
 }
 
 document.querySelector('#retry .startBtn').addEventListener('click', startGame)
+shareResult()
 }
 
 
@@ -371,4 +394,47 @@ function timingCheck(){
     }, 1000);
 }
 
-//TODO: sharing function
+
+function shareResult(){
+const shareBtn = document.getElementById('socialIcons')
+shareBtn.addEventListener('click', (e) =>{
+    if(e.target.tagName == "IMG"){
+        shareScreenshot()
+    }
+})
+}
+
+function shareScreenshot() {
+    html2canvas(document.getElementById('resultMenu'))
+    .then(function(canvas) {
+        canvas.toBlob((blob) => {
+            const url = URL.createObjectURL(blob);
+            const file = new File([blob], 'Result.png', { type: 'image/png' });
+
+            if (navigator.share) {
+                navigator.share({
+                    title: 'Result',
+                    text: 'Check out this Result | App Link:- https://subrataprojects.netlify.app/quiz_app !',
+                    files: [file]
+                })
+                .catch(() => alert('Error sharing Result'));
+            }else showPop(url)
+
+        }, 'image/png');
+    });
+}
+
+function showPop(url){
+    document.getElementById('popUp').style.transform = 'scale(1)'
+    const downloadBtn = document.getElementById('downloadResult')
+        downloadBtn.href = url
+        downloadBtn.download = "Result.png"
+        
+    const closeBtns = document.querySelectorAll('.popClose')
+    closeBtns.forEach((btn) =>{
+        btn.addEventListener('click', (e) =>{
+            e.preventDefault()
+            document.getElementById('popUp').style.transform = 'scale(0)'
+        })
+    })
+}
